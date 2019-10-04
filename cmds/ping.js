e8615d6b3f952680eac73938975f517b20a6c617
Artur <artur@briginvest.ru>
2019-10-04 12:41:10 +0300

    1

---
+++ cmds/ping.js
@@ -0,0 +1,10 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+
+module.exports.run = async (bot, message, args) => {
+    message.channel.send('pong!');
+};
+
+module.exports.help = {
+    name: "ping"
+};
